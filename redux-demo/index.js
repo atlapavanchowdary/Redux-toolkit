de53805c7e443cf4fd101fbd0d1f4a1855b3561c
atlapavanchowdary <atlapavanchowdary.com>
2023-08-23 21:42:39 +0530

    commit

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,20 +1,20 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
-const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_ORDERED = 'CAKE_ORDERED'   // 1. const declaration
 
-function orderCake() {
+function orderCake() {  // 2. action
   return {
     type: CAKE_ORDERED,
     quantity: 1,
   }
 }
 
-const initialState = {
+const initialState = {  // 3. initial state
   numOfCakes: 10,
 }
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {  // 4. reducer
   switch(action.type){
     case CAKE_ORDERED:
       return{
@@ -26,13 +26,13 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer)
-console.log('Initial state',store.getState())
+const store = createStore(reducer)  // 5. store
+console.log('Initial state',store.getState())  // 5.1 getState
 
-const unsubscribe = store.subscribe(() => console.log('update state', store.getState()))
+const unsubscribe = store.subscribe(() => console.log('update state', store.getState())) // 5.2 subscribe
 
+store.dispatch(orderCake())  // 5.3 dispatch (logs the subscribe method)
 store.dispatch(orderCake())
 store.dispatch(orderCake())
-store.dispatch(orderCake())
 
-unsubscribe();
+unsubscribe();  // 5.4 unsubscribe
